0818f148f7fb534335204ac36b177e697a8e0713
Ricky Canepa <rickycanepa.rc@gmail.com>
2022-11-17 16:49:56 -0600

    customers now can only see their own tickets

---
+++ src/components/tickets/TicketList.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react"
+import "./Tickets.css"
+
+export const TicketList = () => {
+    const [tickets, setTickets] = useState([])
+    const [filteredTickets, setFiltered] = useState([])
+
+    const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserObject = JSON.parse(localHoneyUser)
+
+    useEffect(
+        () => {
+            fetch('http://localhost:8088/serviceTickets')
+            .then(response => response.json())
+            .then((ticketArray) => {
+                setTickets(ticketArray)
+            })
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
+
+    useEffect(
+        () => {
+            if (honeyUserObject.staff) {
+                // For employees
+                setFiltered(tickets)
+            }
+            else {
+                // For customers
+                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
+                setFiltered(myTickets)
+            }
+        },
+        [tickets]
+    ) 
+
+    return <> 
+    <h2>List of Tickets</h2>
+
+    <article className="tickets">
+        {
+            filteredTickets.map(
+                (ticket) => {
+                    return <section className="ticket" key={`ticket--${ticket.id}`}>
+                        <header>{ticket.description}</header>
+                        <footer>Emergency: {ticket.emergency ? "🧨" : "No"}</footer>
+                    </section>
+                }
+            )
+        }
+        </article>
+        </>
+}
+
